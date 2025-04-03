8b65b9d35359c37ec59c41eb3561c045c3f0de69
Your Name <you@example.com>
2025-04-03 23:36:02 +0200

    first commit

--- app/api/sendOrder/route.js
+++ app/api/sendOrder/route.js
@@ -6,7 +6,7 @@ export const revalidate = 10;
 export async function POST(request) {
     try {
         const body = await request.json();
-        const { inputs, items, total, delivery, code } = body;
+        const { inputs, items, total, delivery, code, rate, cur} = body;
         const client = await clientPromise; // Connect to MongoDB
         const db = client.db('test'); // Replace with your database name
         const collection = db.collection('Order'); // Replace with your collection name
@@ -22,6 +22,8 @@ export async function POST(request) {
             total: total,
             delivery: delivery+"",
             code: code,
+            rate: rate,
+            cur: cur,
             date: formattedDate, // Added date field
         });
 
